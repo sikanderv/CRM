0f4a08751ea2ea16e643b8e28674ae93f497f1eb
Sikander <sikanderv@gmail.com>
2017-03-17 19:14:55 +0000

    Part-2

---
+++ app/controllers/finder_controller.rb
@@ -0,0 +1,13 @@
+class FinderController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+
+  def alphabetized
+    @customers = Customer.all.order(:full_name)
+  end
+
+  def missing_email
+    @customers = Customer.where("email = ''")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  # get 'finder/index'
+
+  # get 'finder/alphabetized'
+
+  # get 'finder/missing_email'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'finder#index'
+  # resources 'customers', only: [:index]
+  get '/customers/missing_email' => 'finder#missing_email', as: "missing_email"
+  get '/customers/alphabetized' => 'finder#alphabetized', as: "alphabetized"
 end
